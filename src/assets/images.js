f9547589f0f5e693e861bf0a735a4a9e035d7064
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-08-19 16:57:07 +0500

    Commit

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,43 +1,47 @@
 import aboutImage from "../assets/images/deluxe_about_image.png";
+import aboutSecond_Small_device from "../assets/images/aboutSecond_Small_device.png"
+import aboutSecond_image from "../assets/images/aboutSecond_image.png"
+import aboutSecond from "../assets/images/aboutSecond.png"
+
 import control_imageSecond from "../assets/images/fourthSecImageOne.png"
 import control_imageThird from "../assets/images/fourthSecImageThird.png"
 import control_imageFourth from "../assets/images/fourthSecImageTwo.png"
-import aboutSecond_image from "../assets/images/aboutSecond_image.png"
-import aboutSecond from "../assets/images/aboutSecond.png"
-import aboutSecond_Small_device from "../assets/images/aboutSecond_Small_device.png"
 import Black_buliding from "../assets/images/blackBuilding.png"
-// import control_imageTwo from "../assets/images/fourthSecimage.png"
-import controlSec_imageSecond from "../assets/images/fourthSecImageThird.png"
-import controlSec_imageThird from "../assets/images/fourthSecImageThird.png"
+
 import slideOne from '../assets/images/sliderOne.png'
 import slideTwo from "../assets/images/slide_two.png"
 import slideThree from "../assets/images/slider_three.png"
 import slideFour from "../assets/images/sliderFour.png"
+
 import contactImage from "../assets/images/FormImage.jpg"
 import contact_image_second from "../assets/images/contactImage.png"
+
+
 import blogImage from "../assets/images/blogImage.png"
 import blogImageSecond from "../assets/images/blogImageSecond.png"
 import blogImageThird from "../assets/images/blogImageThree.png"
 import blogImageFourth from "../assets/images/blogImageFourth.png"
 import resultLogo from "../assets/images/resultLogo.png"
+
 export default {
     aboutImage,
-    control_imageSecond,
-    control_imageThird,
-    control_imageFourth,
     aboutSecond_image,
     aboutSecond,
     aboutSecond_Small_device,
+
+    control_imageSecond,
+    control_imageThird,
+    control_imageFourth,
     Black_buliding,
-    // control_imageTwo,
-    controlSec_imageThird,
-    controlSec_imageSecond,
+
     slideOne,
     slideTwo,
     slideThree,
     slideFour,
+
     contactImage,
     contact_image_second,
+
     blogImage,
     blogImageSecond,
     blogImageThird,
